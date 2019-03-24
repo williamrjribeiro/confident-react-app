a6444caba7363a8377ec65c2efe597665248e08b
William R. J. Ribeiro <will@williamrjribeiro.com>
2019-03-24 11:42:20 +0100

    Initial commit of List Group component
    - Refactor tests to use Enzyme

--- src/components/listGroup.spec.js
+++ src/components/listGroup.spec.js
@@ -1,7 +1,8 @@
+// @flow
 import React from 'react';
 import { mount } from 'enzyme';
-import App from './App';
+import ListGroup from './ListGroup';
 
 it('renders without crashing', () => {
-  mount(<App />)
+  mount(<ListGroup />)
 });
